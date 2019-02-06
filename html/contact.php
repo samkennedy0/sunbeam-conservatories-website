6a01a55cfd8e28e75f449e5b1640c94ce3d1d708
Sam Kenendy <0samkennedy0@gmail.com>
2019-02-06 16:36:13 +0000

    2nd revsion added number section to conact form, changed layout of header and navbar

--- html/contact.php
+++ html/contact.php
@@ -1,12 +1,13 @@
 <?php
-if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
-	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
+if( isset($_POST['n']) && isset($_POST['u']) && isset($_POST['e']) && isset($_POST['m']) ){
+	$n = $_POST['n'];
+	$u = $_POST['u']; // HINT: use preg_replace() to filter the data
 	$e = $_POST['e'];
 	$m = nl2br($_POST['m']);
 	$to = "samsta1@live.co.uk";	
 	$from = $e;
 	$subject = 'Contact Form Message';
-	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
+	$message = '<b>Name:</b> '.$n.'<br><b>Number:</b> '.$u.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
